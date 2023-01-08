3fbae912b2d2b68be1a6dc153693cb337d58cb25
yanzuc <yanzucchini@gmail.com>
2023-01-08 16:10:19 -0300

    day 6.2

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -15,7 +15,7 @@ const RestaurantCard = ({
   lat,
 }) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity className="bg-white mr-3 shadow">
       <Image
         source={{
           uri: imgUrl,
